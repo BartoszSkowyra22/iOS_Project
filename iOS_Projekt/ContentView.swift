69940045056de55028f7b6a90354e25f2fc3b17f
BartoszSkowyra22 <skowyrab@gmail.com>
2024-05-28 23:29:42 +0200

    Start applictaion

--- iOS_Projekt/ContentView.swift
+++ iOS_Projekt/ContentView.swift
@@ -9,78 +9,145 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    //    @Environment(\.managedObjectContext) private var viewContext
+    //
+    //    @FetchRequest(
+    //        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+    //        animation: .default)
+    //    private var items: FetchedResults<Item>
+    
     @Environment(\.managedObjectContext) private var viewContext
-
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+        sortDescriptors: [NSSortDescriptor(keyPath: \Movie.name, ascending: true)], animation: .default)
+    private var movies: FetchedResults<Movie>
+    
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Category.name, ascending: true)], animation: .default)
+    private var categories: FetchedResults<Category>
+    
+    @State private var movieName: String = ""
+    @State private var movieYear: String = ""
+    @State private var movieDuration: String = ""
+    @State private var movieRating: Double = 0
+    @State private var selectedCategory: Category?
+    //KONIEC przeklejonego
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            VStack {
+                Form {
+                    TextField("Nazwa Filmu", text: $movieName)
+                    TextField("Rok Premiery", text: $movieYear)
+                        .keyboardType(.numberPad)
+                    TextField("Czas Trwania", text: $movieDuration)
+                        .keyboardType(.numberPad)
+                    VStack {
+                        HStack {
+                            Text("Ocena: \(String(format: "%.1f", movieRating))")
+                            Spacer()
+                        }
+                        Slider(value: $movieRating, in: 0...10, step: 0.1)
+                    }
+                    
+                    Picker("Kategoria", selection: $selectedCategory) {
+                        Text("Wybierz").tag(nil as Category?)
+                        ForEach(categories, id: \.self) { category in
+                            Text(category.name ?? "Nieznane").tag(category as Category?)
+                        }
+                    }
+                    if categories.count == 0 {
+                        Button("Dodaj Kategorie") {
+                            addCategory()
+                        }
+                    }
+                    
+                    Button("Dodaj Film") {
+                        addMovie()
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                
+                List {
+                    ForEach(categories) { category in
+                        Section(header: Text("\(category.name ?? "Nieznane")")) {
+                            ForEach(category.moviesArray) { movie in
+                                NavigationLink(destination: EditMovieView(movie: movie)) {
+                                    //MARK: Poprawić wyświetlanie
+                                    Text("\(movie.name ?? "Nieznane") - powstał w \(movie.year)")
+                                }
+                            }
+                            .onDelete(perform: deleteMovie)
+                        }
+                    }
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                .navigationTitle("Filmy")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        EditButton()
                     }
                 }
             }
-            Text("Select an item")
         }
     }
-
-    private func addItem() {
+    
+    private func addMovie() {
+        guard let year = Int16(movieYear), let duration = Int16(movieDuration), let rating = Double?(movieRating), let category = selectedCategory else {
+            return
+        }
+        
+        let newMovie = Movie(context: viewContext)
+        newMovie.name = movieName
+        newMovie.year = year
+        newMovie.duration = duration
+        newMovie.rating = rating
+        newMovie.toCategory = category
+        
+        do {
+            try viewContext.save()
+            movieName = ""
+            movieYear = ""
+            movieDuration = ""
+            movieRating = 0
+            selectedCategory = nil
+        } catch {
+            let nsError = error as NSError
+            fatalError("Nierozpoznany błąd \(nsError), \(nsError.userInfo)")
+        }
+    }
+    
+    private func deleteMovie(offsets: IndexSet) {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
+            offsets.map { movies[$0] }.forEach(viewContext.delete)
+            
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                fatalError("Nierozpoznany błąd \(nsError), \(nsError.userInfo)")
             }
         }
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
+    
+    private func addCategory() {
+        let categories = ["Komedia", "Film romantyczny", "Film akcji", "Horror"]
+        for categoryName in categories {
+            let newCategory = Category(context: viewContext)
+            newCategory.name = categoryName
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                fatalError("Nierozpoznany błąd \(nsError), \(nsError.userInfo)")
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-#Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+private extension Category {
+    var moviesArray: [Movie] {
+        return (toMovie?.allObjects as? [Movie]) ?? []
+    }
 }
+
+
+//#Preview {
+//    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+//}
--- iOS_Projekt/EditMovieView.swift
+++ iOS_Projekt/EditMovieView.swift
@@ -5,14 +5,46 @@
 //  Created by Bartosz Skowyra on 28/05/2024.
 //
 
+import Foundation
 import SwiftUI
+import CoreData
 
 struct EditMovieView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Movie.name, ascending: true)], animation: .default)
+    
+    private var movies: FetchedResults<Movie>
+    var movie:Movie
+    @State var editMovieName: String = ""
+    @State var editMovieYear: String = ""
+    @State var editMovieDuration: String = ""
+    @State var editMovieRating: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            TextField("Podaj nową nazwę filmu", text: $editMovieName)
+            TextField("Podaj nowy rok premiery", text: $editMovieYear)
+                .keyboardType(.numberPad)
+            TextField("Podaj nowy czas trwania", text: $editMovieDuration)
+                .keyboardType(.numberPad)
+            TextField("Podaj nową ocenę", text: $editMovieRating)
+                .keyboardType(.numberPad)
+            
+            Button(action: ({
+                movie.name = self.editMovieName
+                movie.year = Int16(self.editMovieYear) ?? 0
+                movie.duration = Int16(self.editMovieDuration) ?? 0
+                movie.rating = Double(self.editMovieRating) ?? 0
+                //MARK: dog.toBreed = self.dog.toBreed
+                do {
+                    try self.viewContext.save()
+                } catch {
+                    let nsError = error as NSError
+                    fatalError("Nierozpoznany błąd \(nsError), \(nsError.userInfo)")
+                }
+            })) {
+                Text("Edytuj")
+            }
+        }
     }
-}
-
-#Preview {
-    EditMovieView()
 }
